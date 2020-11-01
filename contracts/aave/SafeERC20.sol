db230e9585b47a8e304eb36666c6f21ecd7afacf
CryptoEatsTheWorld <69774383+CryptoEatsTheWorld@users.noreply.github.com>
2020-11-01 15:14:01 -0500

    Update SafeERC20.sol

--- contracts/aave/SafeERC20.sol
+++ contracts/aave/SafeERC20.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.0;
 
 import "./IERC20.sol";
 import "./SafeMath.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/utils/Address.sol";
+import "./Address.sol";
 
 /**
  * @title SafeERC20
