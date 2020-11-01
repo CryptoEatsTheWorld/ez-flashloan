b3dd82b3a78c55a96d8e79e0132d15c804564ecd
CryptoEatsTheWorld <69774383+CryptoEatsTheWorld@users.noreply.github.com>
2020-11-01 15:53:36 -0500

    Update Ownable.sol

--- contracts/aave/Ownable.sol
+++ contracts/aave/Ownable.sol
@@ -62,5 +62,5 @@ contract Ownable is Context {
     //    require(newOwner != address(0), "Ownable: new owner is the zero address");
     //    emit OwnershipTransferred(_owner, newOwner);
     //    _owner = newOwner;
-    }
+    // }
 }
