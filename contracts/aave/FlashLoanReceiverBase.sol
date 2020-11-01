be1db4c75331224f26e6dd2a87348049c4f10671
CryptoEatsTheWorld <69774383+CryptoEatsTheWorld@users.noreply.github.com>
2020-11-01 13:57:21 -0500

    Update FlashLoanReceiverBase.sol

--- contracts/aave/FlashLoanReceiverBase.sol
+++ contracts/aave/FlashLoanReceiverBase.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.5.0;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./SafeMath.sol";
 import "./IERC20.sol";
 import "./SafeERC20.sol";
 import "./IFlashLoanReceiver.sol";
